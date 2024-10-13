0b02ffdde70c2ac68f255a5595cc26bfb0f6bf86
Adidinamite <adrian.aparaschivei04@gmail.com>
2024-10-13 18:03:11 +0300

    Changed navbar, now it has fixed position, avoided overlapping with other sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
 import Navbar from "../components/Navbar.jsx";
+import HomeSection from "../components/HomeSection.jsx";
 const HomePage = () =>{
     return(
-        <Navbar />
+        <>
+            <Navbar />
+            <HomeSection />
+        </>
     );
 };
 export default HomePage;
