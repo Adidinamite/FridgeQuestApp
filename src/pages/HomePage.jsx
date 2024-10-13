a86c55c58fde9bb9aa10561f5e07ccd5fa05ba82
Adidinamite <adrian.aparaschivei04@gmail.com>
2024-10-13 20:50:47 +0300

    Added InfoSection+ made changes to HomeSection

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar.jsx";
 import HomeSection from "../components/HomeSection.jsx";
+import InfoSection from "../components/InfoSection.jsx";
 const HomePage = () =>{
     return(
         <>
             <Navbar />
             <HomeSection />
+            <InfoSection/>
         </>
     );
 };
