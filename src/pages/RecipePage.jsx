6c7ae5e56bc07694de7fb49cc482a7ae3eaae8a0
Adidinamite <adrian.aparaschivei04@gmail.com>
2024-10-14 11:38:48 +0300

    ingredientsList are taken from IngredientsSection to RecipeSection using Lifting Up State method

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,11 +1,24 @@
 import Navbar from "../components/Navbar.jsx";
 import IngredientsSection from "../components/IngredientsSection.jsx";
-
+import RecipeSection from "../components/RecipeSection.jsx";
+import { useState } from "react";
 const RecipePage = () => {
+    const [ingredientsList, setIngredientsList] = useState([]);
+    const addIngredient = (ingredient) => {
+        setIngredientsList([...ingredientsList, ingredient]);
+    };
+    const deleteIngredient = (index) => {
+        setIngredientsList(ingredientsList.filter((_, i) => i !== index));
+    };
     return(
         <>
             <Navbar/>
-            <IngredientsSection/>
+            <IngredientsSection
+                ingredientsList={ingredientsList}
+                addIngredient={addIngredient}
+                deleteIngredient={deleteIngredient}
+            />
+            <RecipeSection ingredientsList={ingredientsList} />
         </>
     );
 };
