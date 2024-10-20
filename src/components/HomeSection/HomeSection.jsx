2167bb4c70a6615b48855280c28344db183be39b
Adidinamite <adrian.aparaschivei04@gmail.com>
2024-10-20 18:09:18 +0300

    added go-to \recipe button

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -1,37 +1,41 @@
+import { useNavigate } from "react-router-dom";
 import "./HomeSection.css";
 
 const HomeSection = () => {
+  const navigate = useNavigate();
+
   const BulletPointList = ({ items }) => {
     return (
-      <ul className="bulletPoints">
-        {items.map((item, index) => (
-          <li key={index}>
-            <strong>{item.title}:</strong> {item.description}
-          </li>
-        ))}
-      </ul>
+        <ul className="bulletPoints">
+          {items.map((item, index) => (
+              <li key={index}>
+                <strong>{item.title}:</strong> {item.description}
+              </li>
+          ))}
+        </ul>
     );
   };
+
   const keyFeatures = [
     {
       title: "Ingredient-based recipes",
       description:
-        "Input the ingredients you have, and FridgeQuest will generate a recipe for you.",
+          "Input the ingredients you have, and FridgeQuest will generate a recipe for you.",
     },
     {
       title: "Powered by ChatGPT",
       description:
-        "Uses the powerful ChatGPT API to create step-by-step recipes tailored to your ingredients.",
+          "Uses the powerful ChatGPT API to create step-by-step recipes tailored to your ingredients.",
     },
     {
       title: "Reduces food waste",
       description:
-        "Make the most of the ingredients you already have, minimizing waste.",
+          "Make the most of the ingredients you already have, minimizing waste.",
     },
     {
       title: "Personalized and creative",
       description:
-        "Whether you are an experienced chef or a busy individual, FridgeQuest helps spark creativity in the kitchen.",
+          "Whether you are an experienced chef or a busy individual, FridgeQuest helps spark creativity in the kitchen.",
     },
   ];
 
@@ -39,7 +43,7 @@ const HomeSection = () => {
     {
       title: "Time-saving",
       description:
-        "Quickly generate recipes without searching for new ones online.",
+          "Quickly generate recipes without searching for new ones online.",
     },
     {
       title: "Fun and interactive",
@@ -51,25 +55,37 @@ const HomeSection = () => {
     },
   ];
 
-  return (
-    <section id="about" className="content">
-      <div className="leftContainer">
-        <h1>Do not know what to cook?</h1>
-        <h2>FridgeQuest is here to help!</h2>
-        <p>
-          FridgeQuest is a web app which comes in help with how you cook by
-          making the most out of ingredients you already have at home.
-        </p>
-        <h2>Key Features:</h2>
-        <BulletPointList items={keyFeatures} />
+  const goToRecipes = () => {
+    navigate("/recipes");
+  };
 
-        <h2>Why Use FridgeQuest?</h2>
-        <BulletPointList items={whyUseFridgeQuest} />
-      </div>
-      <div className="rightContainer">
-        <img src="/images/fridgeQuest.png" alt="FridgeQuest" />
-      </div>
-    </section>
+  return (
+      <section id="about" className="content">
+        <div className="leftContainer">
+          <h1>Do not know what to cook?</h1>
+          <h2>FridgeQuest is here to help!</h2>
+          <p>
+            FridgeQuest is a web app which comes in help with how you cook by
+            making the most out of ingredients you already have at home.
+          </p>
+          <h2>Key Features:</h2>
+          <BulletPointList items={keyFeatures} />
+
+          <h2>Why Use FridgeQuest?</h2>
+          <BulletPointList items={whyUseFridgeQuest} />
+        </div>
+        <div className="rightContainer">
+          <img src="/images/fridgeQuest.png" alt="FridgeQuest" />
+          <div className={"recipeRedirect"}>
+            <h2>
+              Get started now!
+            </h2>
+            <button className="recipeButton" onClick={goToRecipes}>
+              Generate Recipes
+            </button>
+          </div>
+        </div>
+      </section>
   );
 };
 
